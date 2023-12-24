636ff34d105e390e3c0940b1586ac4e81a0bddf7
Bouchellal Imad Eddine <124831129+Imad-Bchl@users.noreply.github.com>
2023-12-24 22:47:46 +0100

    cards and pop ups

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './assets/PopDeleteArticle.jsx'
+import App2 from './assets/PopLogOut.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <App2 />
   </React.StrictMode>,
 )
