d7c3f1439b205d6d3fcdff4ce2240fc4f7d65a31
yacine1240 <khellas.yacine1@gmail.com>
2023-12-25 12:13:13 +0100

    404 error page added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { AnimatePresence } from "framer-motion";
 import Layout from "./components/Layout";
 import { UserProvider } from "./contexts/UserContext";
 import PrivateRoute from "./routes/PrivateRoute";
+import { routes } from "./routes/routes";
+import Error404 from "./components/Error404";
 
 function App() {
   const location = useLocation();
@@ -14,9 +16,10 @@ function App() {
       <UserProvider>
         <AnimatePresence mode="wait">
           <Routes key={location.pathname} location={location}>
-            <Route exact path="/" element={<Header />} />
-            <Route exact path="/signin" element={<SignIn />} />
-            <Route exact path="/signup" element={<SignUp />} />
+            <Route path="*" element={<Error404 />} />
+            <Route exact path={routes.HEADER} element={<Header />} />
+            <Route exact path={routes.SIGNIN} element={<SignIn />} />
+            <Route exact path={routes.SIGNUP} element={<SignUp />} />
             <Route exact path="/" element={<PrivateRoute />}>
               <Route exact path="layout" element={<Layout title={"Home"} />} />
             </Route>
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+  HEADER: "/",
+  SIGNIN: "/signin",
+  SIGNUP: "/signup",
+  HOME: "home",
+  FAVORITE: "favoris",
+  HISTORY: "history",
+  CATEGORIES: "categories",
+};
