5e1fd3da56763d21e81d5094a0d30329f735c5d9
Bouchellal Imad Eddine <124831129+Imad-Bchl@users.noreply.github.com>
2024-02-01 23:21:28 +0100

    responsivity

--- src/components/ArticleDeatails.jsx
+++ src/components/ArticleDeatails.jsx
@@ -28,18 +28,18 @@ function ArticleDetails(){
 
   return (
     <div className="w-full">
-        <div className="W-4/5 pb-4 px-6 flex flex-col flex-wrap justify-center content-center bg-[#DEDEDE] font-poppins">
+        <div className="pb-4 sm:px-6 flex flex-col flex-wrap justify-center content-center bg-[#DEDEDE] font-poppins">
             <div className="flex justify-center sm:mb-3 mb-5">
-                <h1 className="sm:text-3xl text-6xl font-meduim text-center px-20 my-7"> {article.title}</h1>
+                <h1 className="sm:text-3xl text-xl font-meduim text-center sm:mx-20 my-7 w-5/6"> {article.title}</h1>
             </div>
 
-            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-4"/>
+            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-2"/>
 
-            <div className="flex justify-center flex-col sm:px-4 px-6 my-5">
+            <div className="flex justify-center flex-col sm:px-4 px-6 sm:my-5 py-1">
                 <div className="flex content-center flex-nowrap my-3">
-                    <LiaPenFancySolid  className="sm:h-6 h-10 sm:w-6 w-10 sm:mr-1 mr-2"/>
-                    <p className='text-[#190B28] text-3xl sm:text-base font-semibold mr-1'>Auteurs:</p>
-                    <p className='text-[#190B28] text-3xl sm:text-base '>
+                    <LiaPenFancySolid  className="h-6 w-6 mr-1"/>
+                    <p className='text-[#190B28] text-base font-semibold mr-1'>Auteurs:</p>
+                    <p className='text-[#190B28] text-base '>
                         {article.authors.map(auteur =>{
                                 return(auteur.name + ". ")
                         })};
@@ -47,9 +47,9 @@ function ArticleDetails(){
                 </div>
 
                 <div className="flex content-center flex-nowrap">
-                    <LiaPenFancySolid  className="sm:h-6 sm:w-6 h-10 w-10 sm:mr-1 mr-2"/>
-                    <p className='text-[#190B28] text-3xl sm:text-base font-semibold mr-1'>Institutions:</p>
-                    <p className='text-[#190B28] text-3xl sm:text-base '>
+                    <LiaPenFancySolid  className="h-6 w-6 mr-1"/>
+                    <p className='text-[#190B28] text-base font-semibold mr-1'>Institutions:</p>
+                    <p className='text-[#190B28] text-base '>
                         {article.authors.map(auteur =>{
                             return(auteur.institutions.map(institution=>{
                                 return(institution.institutions_name + " ")
@@ -59,9 +59,9 @@ function ArticleDetails(){
                 </div>
 
                 <div className="flex content-center flex-nowrap my-3">
-                    <LiaPenFancySolid  className="sm:h-6 sm:w-6 h-10 w-10 sm:mr-1 mr-2"/>
-                    <p className='text-[#190B28] text-3xl sm:text-base font-semibold mr-1'>Mots clés:</p>
-                    <p className='text-[#190B28] text-3xl sm:text-base '>
+                    <LiaPenFancySolid  className="h-6 w-6 mr-1"/>
+                    <p className='text-[#190B28] text-basefont-semibold mr-1'>Mots clés:</p>
+                    <p className='text-[#190B28] text-base'>
                         {article.keywords.map(keyword => {
                             return(keyword + ", ")
                                 }
@@ -70,36 +70,42 @@ function ArticleDetails(){
                 </div>
             </div>
 
-            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-4"/>
+            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-2"/>
 
-            <div className="flex justify-center my-5 flex-col sm:px-4 px-6 py-2">
-                <p className="text-5xl sm:text-3xl font-meduim sm:my-2 mb-3 mx-1">Abstract:</p>
-                <p className='text-[#190B28] text-3xl sm:text-sm text-justify mt-2 mb-5'>{article.abstract} </p>
+            <div className="flex justify-center my-1 flex-col sm:px-4 px-6 py-2">
+                <p className="text-2xl sm:text-3xl font-meduim sm:my-2 mb-3 mx-1">Abstract:</p>
+                <p className='text-[#190B28] text-base sm:text-sm text-justify mt-2 mb-5 w-full'>{article.abstract} </p>
             </div>
 
-            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-4"/>
+            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-2"/>
 
             <div className="flex justify-center mb-3 flex-col sm:px-4 px-6 py-2 mt-3">
-                <p className="text-5xl sm:text-3xl font-meduim sm:my-2 mb-4 mx-1">Article:</p>
-                <div className="grid grid-cols-2 gap-10 px-2 mb-5 mt-6">
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm text-justify' >{displayWordRange(texte,0,len6+1)}</p></div>
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(len6)+1,(2*len6)+1)}</p></div>
+                <p className="text-2xl sm:text-3xl font-meduim sm:my-2 mx-1">Article:</p>
+                <div className="sm:grid grid-cols-2 gap-10 px-2 mb-5 sm:mt-6 mt-4 hidden">
+                    <div><p className='text-[#190B28] text-base sm:text-sm text-justify' >{displayWordRange(texte,0,len6+1)}</p></div>
+                    <div><p className='text-[#190B28] text-base sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(len6)+1,(2*len6)+1)}</p></div>
                 </div>
-                <div className="grid grid-cols-2 gap-10 px-2 mb-5">
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm text-justify' >{displayWordRange(texte,(2*len6)+1,(3*len6)+1)}</p></div>
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(3*len6)+1,(4*len6)+1)}</p></div>
+                <div className="sm:grid grid-cols-2 gap-10 px-2 mb-5 hidden">
+                    <div><p className='text-[#190B28] text-base sm:text-sm text-justify' >{displayWordRange(texte,(2*len6)+1,(3*len6)+1)}</p></div>
+                    <div><p className='text-[#190B28] text-base sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(3*len6)+1,(4*len6)+1)}</p></div>
                 </div>
-                <div className="grid grid-cols-2 gap-10 px-2 mb-7">
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm text-justify' >{displayWordRange(texte,(4*len6)+1,(5*len6)+1)}</p></div>
-                    <div><p className='text-[#190B28] text-3xl sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(5*len6)+1,len+1)}</p></div>
+                <div className="sm:grid grid-cols-2 gap-10 px-2 mb-7 hidden">
+                    <div><p className='text-[#190B28] text-base sm:text-sm text-justify' >{displayWordRange(texte,(4*len6)+1,(5*len6)+1)}</p></div>
+                    <div><p className='text-[#190B28] text-base sm:text-sm col-span-1 text-justify' > {displayWordRange(texte,(5*len6)+1,len+1)}</p></div>
                 </div>
+
+                {/* phone view */}
+                <div className="px-2 mb-2 sm:hidden sm:mt-6 mt-4">
+                    <div><p className='text-[#190B28] text-base sm:text-sm text-justify' >{texte}</p></div>
+                </div>
+                
             </div>
 
-            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-4"/>
+            <hr className="bg-[#E87D00] h-1 sm:h-0.5 w-full rounded-full border-none sm:mb-0 mb-2"/>
 
-            {/* <div className="my-8 flex justify-center h-[95vh]">
+            <div className="my-8 flex justify-center h-[95vh]">
                 <iframe src={article.pdf_url} width="95%" height="100%"/>
-            </div> */}
+            </div>
         </div>
     </div>
   );
--- src/components/ModifierModerateur.jsx
+++ src/components/ModifierModerateur.jsx
@@ -68,19 +68,19 @@ function ModifierModerateur(){
     return (
         <div className="w-full relative">
             {Display && <Pop Close={Close} Confirmer={handleSubmitPop} />}
-            <div className="w-full py-4 px-6 flex flex-col flex-wrap justify-center content-center bg-[#dedede] font-poppins">
+            <div className="w-full py-4 sm:px-6 flex flex-col flex-wrap justify-center content-center bg-[#dedede] font-poppins">
                 <form autoComplete="off" action="post" className="w-full flex flex-col flex-wrap justify-center content-center">
-                    <p className=" font-poppins text-xs pl-1 text-[#190B28] my-1">Nom:</p>
-                    <input type="text" defaultValue={moder.firstName} onChange={handleChangeNom} className="py-2.5 pl-8 w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
+                    <p className=" font-poppins text-xs pl-1 text-[#190B28] my-1 sm:w-1/2 w-5/6">Nom:</p>
+                    <input type="text" defaultValue={moder.firstName} onChange={handleChangeNom} className="py-2.5 sm:pl-8 pl-4 sm:w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
                     <p className=" font-poppins text-xs pl-1 text-[#190B28] my-1">Prenom:</p>
-                    <input type="text" defaultValue={moder.lastName} onChange={handleChangePreom} className="py-2.5 pl-8 w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
+                    <input type="text" defaultValue={moder.lastName} onChange={handleChangePreom} className="py-2.5 sm:pl-8 pl-4 sm:w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
                     <p className=" font-poppins text-xs pl-1 text-[#190B28] my-1">Nom d'utilisateur:</p>
-                    <input type="text" defaultValue={moder.username} onChange={handleChangeSurnom} className="py-2.5 pl-8 w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
+                    <input type="text" defaultValue={moder.username} onChange={handleChangeSurnom} className="py-2.5 sm:pl-8 pl-4 sm:w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-4"/>
                     <p className=" font-poppins text-xs pl-1 text-[#190B28] my-1">Email:</p>
-                    <input type="text" defaultValue={moder.email} onChange={handleChangeMail} className="py-2.5 pl-8 w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-10"/>
+                    <input type="text" defaultValue={moder.email} onChange={handleChangeMail} className="py-2.5 sm:pl-8 pl-4 sm:w-1/2 rounded-full bordre-none bg-[#E87D00] bg-opacity-25 text-base mb-10"/>
                     <div className='flex justify-evenly mb-4'>
-                        <button onClick={handleAnnuler} className='lg:text-lg text-sm font-poppins font-bold text-white lg:py-2.5 py-2 lg:px-6 px-5 rounded-[20px] shadow-md shadow-[rgba(0,0,0,0.25)] bg-[#A7A7A7]'>Annuler</button>
-                        <button onClick={handleSubmit} className='lg:text-lg text-sm font-poppins font-bold text-white lg:py-2.5 py-2 lg:px-6 px-5 rounded-[20px] shadow-md shadow-[rgba(0,0,0,0.25)] bg-[#E87D00]'>Enregistrer</button>
+                        <button onClick={handleAnnuler} className='lg:text-lg text-sm font-poppins font-bold text-white lg:py-2.5 py-2 lg:px-6 px-4 rounded-[20px] shadow-md shadow-[rgba(0,0,0,0.25)] bg-[#A7A7A7]'>Annuler</button>
+                        <button onClick={handleSubmit} className='lg:text-lg text-sm font-poppins font-bold text-white lg:py-2.5 py-2 lg:px-6 px-4 rounded-[20px] shadow-md shadow-[rgba(0,0,0,0.25)] bg-[#E87D00]'>Enregistrer</button>
                     </div>
                 </form>
             </div>
